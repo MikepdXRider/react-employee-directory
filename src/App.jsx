2d47e28f33626f88952656363641184ebf9aec38
Michael Rider <mikepdxrider@gmail.com>
2022-01-05 16:29:31 -0800

    adds router architecture

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,36 @@
 // Wrap appropriate components in PrivateRoute component
 // 
 
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      {/* Layout component here */}
+        <Switch>
+          <Route exact path='/'>
+              {/* view component */}
+          </Route>
+          <Route path='/login'>
+              {/* view component */}
+          </Route>
+          <Route path='/register'>
+              {/* view component */}
+          </Route>
+          {/* will be private */}
+          <Route path='/create-profile'>
+              {/* view component */}
+          </Route>
+          {/* will be private */}
+          <Route path='/edit-profile'>
+              {/* view component */}
+          </Route>
+          {/* will be private */}
+          <Route path='/profile'>
+              {/* view component */}
+          </Route>
+        </Switch>
+      {/* Layout component here */}
+    </BrowserRouter>
+  )
 }
