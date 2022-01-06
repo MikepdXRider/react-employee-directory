6b83f3c2d30d8517c990fb0662f08582b4eb4aef
Michael Rider <mikepdxrider@gmail.com>
2022-01-05 18:14:04 -0800

    cleans up layout comments

--- src/views/Layout/Layout.jsx
+++ src/views/Layout/Layout.jsx
@@ -1,14 +1,3 @@
-// 
-// Layout View Component
-// Needs access to auth/user context
-// Contains header
-//  - Header contains:
-//      - Acme Inc. name
-//      - A “Sign In” button if the user is signed out
-//      - A “You are signed in as ${email}”` message with a “Sign Out” button if the user is signed in
-// 
-
-
 import React from 'react'
 import { useState } from 'react';
 import { Redirect, useHistory } from 'react-router-dom';
@@ -17,20 +6,11 @@ import useAuth from '../../hooks/useAuth.jsx';
 export default function Layout({ children }) {
     const history = useHistory();
     
-    // destructure user from useAuth hook
     const { user, setUser } = useAuth();
     
-    // // Temporary Mock
-    // let mockUser = {
-    //     name: 'bobby',
-    //     birthday: new Date(),
-    //     bio: 'brief bio',
-    //     email: 'bobby@acme.com'
-    // }
-    // // Temporary Mock
-    // const [user, setUser] = useState(mockUser);
     // Temporary Mocks
     function handleLogout() {
+        // This needs to be an async call to signout the user from useAuth hook
         setUser({});
     }
 
@@ -38,7 +18,6 @@ export default function Layout({ children }) {
         <>
             <header>
                 <h1>Acme Inc</h1>
-                {/* “Sign In” button or “You are signed in as ${email}” message with a “Sign Out” button */}
                 {
                     user.email
                     ? <>
