f678c9c328abe798489f44d7c0ce35be45b1fcb8
Michael Rider <mikepdxrider@gmail.com>
2022-01-05 17:22:59 -0800

    adds home view and layout clientless functionality

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -5,11 +5,15 @@
 // 
 
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min'
 
 export default function Home() {
     return (
-        <div>
-            Welcome Home
-        </div>
+        <section>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus a perferendis modi nesciunt enim temporibus, et veritatis ea quos incidunt ab vero quam laudantium voluptates nisi tempore recusandae fugit? Et?</p>
+            <Link to='/login'>Sign-In</Link>
+            <Link to='/register'>Make account</Link>
+        </section>
     )
 }
--- src/views/Layout/Layout.jsx
+++ src/views/Layout/Layout.jsx
@@ -10,13 +10,40 @@
 
 
 import React from 'react'
+import { useState } from 'react';
+import { Redirect, useHistory } from 'react-router-dom';
 
 export default function Layout({ children }) {
+    const history = useHistory();
+    
+    // destructure user from useAuth hook
+    
+    // Temporary Mock
+    let mockUser = {
+        name: 'bobby',
+        birthday: new Date(),
+        bio: 'brief bio',
+        email: 'bobby@acme.com'
+    }
+    // Temporary Mock
+    const [user, setUser] = useState(mockUser);
+
+    function handleLogout() {
+        setUser({});
+    }
+
     return (
         <>
             <header>
                 <h1>Acme Inc</h1>
                 {/* “Sign In” button or “You are signed in as ${email}” message with a “Sign Out” button */}
+                {
+                    user.email
+                    ? <>
+                        <span>You are signed in as {user.email}</span>
+                        <button onClick={() => handleLogout()}>Sign Out</button> 
+                    </> : <button onClick={() => history.push('/login')}>Sign In</button>
+                }
             </header>
             <main>
                 {children}
